5ddf4c1b236a9c83c6bcf2c5156eca722143985d
oncomouse <andrew@pilsch.com>
2014-03-05 11:47:29 -0700

    As this is a teaching site, I turned off minify_javascript and minify_css in config.rb.

--- config.rb
+++ config.rb
@@ -54,10 +54,10 @@ configure :build do
   #set :http_prefix, "/my/prefix/folder"
 
   # For example, change the Compass output style for deployment
-  activate :minify_css
+  #activate :minify_css
 
   # Minify Javascript on build
-  activate :minify_javascript
+  #activate :minify_javascript
 
   #activate :gzip
   
