cd4b53cef1e2695428cbadabd95242e293b71567
oncomouse <andrew@pilsch.com>
2014-03-05 11:47:22 -0700

    Automated commit at 2014-03-05 18:47:22 UTC by middleman-deploy 0.2.3

--- week3/javascripts/site.js
+++ week3/javascripts/site.js
@@ -1,1 +1,90 @@
-function ready(t,e,a){var d={},n={};a.forEach(function(t){d[t.id]=+t.rate,n[t.id]=t.name}),svg.append("g").attr("class","county").selectAll("path").data(topojson.feature(e,e.objects.counties).features).enter().append("path").attr("d",path).style("fill",function(t){return color(d[t.id])}).style("opacity",.8).on("mouseover",function(t){d3.select(this).transition().duration(300).style("opacity",1),div.transition().duration(300).style("opacity",1),div.text(n[t.id]+" : "+d[t.id]).style("left",d3.event.pageX+"px").style("top",d3.event.pageY-30+"px")}).on("mouseout",function(){d3.select(this).transition().duration(300).style("opacity",.8),div.transition().duration(300).style("opacity",0)})}var width=960,height=500,color_domain=[500,1e3,1500,2e3,2500,3e3,3500,4e3,4500,5e3,5500,6e3],ext_color_domain=[0,500,1e3,1500,2e3,2500,3e3,3500,4e3,4500,5e3,5500,6e3],legend_labels=["< 500","500+","1000+","1500+","2000+","2500+","3000+","3500+","4000+","4500+","5000+","5500+","6000+"],color=d3.scale.threshold().domain(color_domain).range(["#dcdcdc","#d0d6cd","#bdc9be","#aabdaf","#97b0a0","#84a491","#719782","#5e8b73","#4b7e64","#387255","#256546","#125937","#004d28"]),div=d3.select("body").append("div").attr("class","tooltip").style("opacity",0),svg=d3.select("body").append("svg").attr("width",width).attr("height",height).style("margin","10px auto"),path=d3.geo.path();queue().defer(d3.json,"data/us.json").defer(d3.csv,"data/data.csv").await(ready);var legend=svg.selectAll("g.legend").data(ext_color_domain).enter().append("g").attr("class","legend"),ls_w=20,ls_h=20;legend.append("rect").attr("x",20).attr("y",function(t,e){return height-e*ls_h-2*ls_h}).attr("width",ls_w).attr("height",ls_h).style("fill",function(t){return color(t)}).style("opacity",.8),legend.append("text").attr("x",50).attr("y",function(t,e){return height-e*ls_h-ls_h-4}).text(function(t,e){return legend_labels[e]});
+ var width = 960,
+ height = 500;
+ 
+ var color_domain = [500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000]
+ var ext_color_domain = [0, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000]
+ var legend_labels = ["< 500", "500+", "1000+", "1500+", "2000+", "2500+", "3000+", "3500+", "4000+", "4500+", "5000+", "5500+", "6000+"]
+ 
+var color = d3.scale.threshold()
+             .domain(color_domain)
+             .range(["#dcdcdc", "#d0d6cd", "#bdc9be", "#aabdaf", "#97b0a0", "#84a491", "#719782", "#5e8b73", "#4b7e64", "#387255", "#256546", "#125937", "#004d28"]);
+
+var div = d3.select("body").append("div")
+            .attr("class", "tooltip")
+            .style("opacity", 0);
+
+var svg = d3.select("body").append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .style("margin", "10px auto");
+
+var path = d3.geo.path();
+
+/*d3.json("/data/nycc.json", function(error, topology) {
+	svg.append("g")
+		.attr("class", "county")
+		.selectAll("path")
+		.data(topojson.feature(topology, topology.objects.nycc).features)
+		.enter().append("path")
+		.attr("d", path);
+});*/
+
+
+queue()
+ .defer(d3.json, "data/us.json")
+ .defer(d3.csv, "data/data.csv")
+ .await(ready);
+
+function ready(error, us, data) {
+	var pairRateWithId = {};
+	var pairNameWithId = {};
+
+	data.forEach(function(d) {
+		pairRateWithId[d.id] = +d.rate;
+		pairNameWithId[d.id] = d.name;
+	});
+	svg.append("g")
+	   .attr("class", "county")
+	   .selectAll("path")
+	   .data(topojson.feature(us, us.objects.counties).features)
+	   .enter().append("path")
+	   .attr("d", path)
+	   .style ( "fill" , function (d) {
+		   return color (pairRateWithId[d.id]);
+	   })
+	   .style("opacity", 0.8)
+	   .on("mouseover", function(d) {
+			d3.select(this).transition().duration(300).style("opacity", 1);
+			div.transition().duration(300)
+			   .style("opacity", 1);
+			   
+			div.text(pairNameWithId[d.id] + " : " + pairRateWithId[d.id])
+			   .style("left", (d3.event.pageX) + "px")
+			   .style("top", (d3.event.pageY -30) + "px");
+	   })
+	   .on("mouseout", function(d) {
+			d3.select(this).transition().duration(300).style("opacity", 0.8);
+			div.transition().duration(300)
+			   .style("opacity", 0);
+	   });
+};
+
+var legend = svg.selectAll("g.legend")
+ .data(ext_color_domain)
+ .enter().append("g")
+ .attr("class", "legend");
+
+var ls_w = 20, ls_h = 20;
+
+legend.append("rect")
+ .attr("x", 20)
+ .attr("y", function(d, i){ return height - (i*ls_h) - 2*ls_h;})
+ .attr("width", ls_w)
+ .attr("height", ls_h)
+ .style("fill", function(d, i) { return color(d); })
+ .style("opacity", 0.8);
+
+legend.append("text")
+ .attr("x", 50)
+ .attr("y", function(d, i){ return height - (i*ls_h) - ls_h - 4;})
+ .text(function(d, i){ return legend_labels[i]; });
